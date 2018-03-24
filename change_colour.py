d71032a82972b590940fd01a9377b499f190cbf4
Jessica <jessica0greene@gmail.com>
2018-03-24 21:35:08 +0100

    initial commit

--- change_colour.py
+++ change_colour.py
@@ -14,8 +14,9 @@ except:
 import lcd160cr
 lcd = lcd160cr.LCD160CR('X')
 
-colours =[(255,0,0),(255, 140, 26),(255, 255, 0),(0, 230, 0),(0, 255, 255),(0, 0, 255)]
-levels =[32,29,26,23,20,17]
+colours = [(255,0,0),(255, 140, 26),(255, 255, 0),(0, 230, 0),(0, 255, 255),(0, 0, 255)]
+temp_levels = [32,29,26,23,20,17]
+humidity_levels = [80, 70, 60, 50, 40,20]
 
 dt = 20
 b1 = bytearray(1)
@@ -34,33 +35,46 @@ def hdc_temp():
     t = hdc1080_read(0)
     return (t / 0x10000)*165-40
 
-#calculating humidity    
+#calculating humidity
 def hdc_hum():
     t = hdc1080_read(1)
     return (t / 0x10000)*100
-    
-def calculate_color(value):
+
+def calculate_color(value, levels):
     for i in range(len(levels)):
         if value > levels[i]:
             return i
 
-def print_to_display():
-    temp = hdc_temp()
+def print_to_display(temp, humidity):
     lcd.erase()
     lcd.set_orient(lcd160cr.PORTRAIT)
     lcd.set_pos(20, 20)
-    lcd.set_text_color(lcd.rgb(*colours[calculate_color(temp)]), lcd.rgb(0, 0, 0))
+    lcd.set_text_color(lcd.rgb(*colours[calculate_color(temp, temp_levels)]), lcd.rgb(0, 0, 0))
     lcd.set_font(1, 0, 0, 0, scroll=1)
     lcd.write("Temperature(°C)")
     lcd.set_pos(50, 50)
     lcd.set_font(1, 0, 0, 0, 0)
     lcd.write("%.2f" % (temp))
-    lcd.set_text_color(lcd.rgb(255,255,255), lcd.rgb(0, 0, 0)) # star unpacks the colour tuple into the rgb
+    lcd.set_text_color(lcd.rgb(*colours[calculate_color(humidity, humidity_levels)]), lcd.rgb(0, 0, 0)) # star unpacks the colour tuple into the rgb
     lcd.set_pos(30, 100)
     lcd.set_font(1, 0, 0, 0, scroll=1)
     lcd.write("Humidity(%)")
     lcd.set_pos(50, 130)
     lcd.set_font(1, 0, 0, 0, 0)
-    lcd.write("%.2f" % (hdc_hum()))
+    lcd.write("%.2f" % (humidity))
    #print("Both sensors read at once:    %.2f  %.2f" % hdc.temp_hum())
    #print("Battery low: %s" % (hdc.battery_low()))
+
+def check_read_out():
+    current_temp = hdc_temp()
+    current_hum = hdc_hum()
+    while True:
+        new_temp = hdc_temp()
+        new_hum = hdc_hum()
+        #if new_temp > current_temp + 2 or new_temp < current_temp -2 or new_humidity > current_humidity + 10 or new_humidity < current_humidity-10:
+        if new_temp > current_temp + 1 or new_temp < current_temp -1 :
+            print_to_display(current_temp, current_hum)
+            current_temp = new_temp
+            current_hum = new_hum
+        pyb.delay(1000)
+
--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 # main.py -- put your code here!
-from change_colour import print_to_display
+from change_colour import hdc_temp, hdc_hum,  print_to_display
 import pyb
-sw = pyb.Switch()
+import utime
 import lcd160cr
 lcd = lcd160cr.LCD160CR('X')
 uart = pyb.UART('XA', 115200)
 pyb.repl_uart(uart)
-#sw.callback(hdc_temp)
+sw = pyb.Switch()
 
-#usr_pressed = False
+usr_pressed = False
 def usr_pressed_check():
     global usr_pressed
     usr_pressed = True
 
-#sw.callback(usr_pressed_check)
+sw.callback(usr_pressed_check)
 
-while True: 
-    print_to_display()
-    pyb.delay(1000)
+while True:
+    #check_read_out()
+    #pyb.delay(1000)
+    current_temp = hdc_temp()
+    current_hum = hdc_hum()
+    while True:
+        new_temp = hdc_temp()
+        new_hum = hdc_hum()
+        if new_temp > current_temp+1 or new_temp < current_temp-1:
+            current_temp = new_temp
+            current_hum = new_hum
+            print_to_display(current_temp, current_hum)
+        utime.sleep_ms(100)
+    
+    
+    
     #if usr_pressed:
         #read_sensors()
         #usr_pressed = False
