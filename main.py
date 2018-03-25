66d1b3e0e09a32bdefa3b21a45d004e1d0fb5f5f
Jessica <jessica0greene@gmail.com>
2018-03-25 21:32:38 +0200

    changes to main for logging

--- main.py
+++ main.py
@@ -7,19 +7,46 @@ lcd = lcd160cr.LCD160CR('X')
 uart = pyb.UART('XA', 115200)
 pyb.repl_uart(uart)
 sw = pyb.Switch()
+rtc = pyb.RTC()
 
 usr_pressed = False
+
 def usr_pressed_check():
     global usr_pressed
     usr_pressed = True
 
 sw.callback(usr_pressed_check)
 
+def writeLog(rtc, temp, hum):
+    """Append a line with the current timestamp to the log file"""
+    datetime=rtc.datetime()
+    timestamp = ("%04d-%02d-%02d %02d:%02d:%02d" % (datetime[0],
+    datetime[1], datetime[2], datetime[4], datetime[5], datetime[6]))
+    logline = ("%s %s %s" % (timestamp, temp, hum))
+    #print(logline)
+    try:
+        with open("/sd/logdata.txt", "a") as f:
+            f.write("%s\n" % logline)
+            f.close()
+            pyb.sync()
+    except:
+        pass
+    #try:
+        #with open("/flash/logdata.txt", "a") as f:
+           # f.write("%s\n" % logline)
+           # f.close()
+           # pyb.sync()
+
+    #except OSError as error:
+        #print("Error: can not write to SD card. %s" % error)
+
+
 while True:
     #check_read_out()
     #pyb.delay(1000)
     current_temp = hdc_temp()
     current_hum = hdc_hum()
+    print_to_display(current_temp, current_hum)
     while True:
         new_temp = hdc_temp()
         new_hum = hdc_hum()
@@ -27,10 +54,9 @@ while True:
             current_temp = new_temp
             current_hum = new_hum
             print_to_display(current_temp, current_hum)
+        writeLog(rtc,new_temp,new_hum)
         utime.sleep_ms(100)
-    
-    
-    
+
     #if usr_pressed:
         #read_sensors()
         #usr_pressed = False
